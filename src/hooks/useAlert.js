0fbc455ff84c7bac49d116a69b1fdcfd5a36d66c
Cedric Rollo <cedric.rolllo@iut-tlse3.fr>
2023-12-05 15:49:26 +0100

    :tada: init

---
+++ src/hooks/useAlert.js
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+const useAlert = () => {
+  const [alert, setAlert] = useState({
+    show: false,
+    text: 'coucou bibi',
+    type: 'danger',
+  })
+
+  const showAlert = ({ text, type = 'danger' }) =>
+    setAlert({ show: true, text, type })
+  const hideAlert = () => setAlert({ show: false, text: '', type: 'danger' })
+
+  return { alert, showAlert, hideAlert }
+}
+
+export default useAlert
